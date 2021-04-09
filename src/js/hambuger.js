e775dbba73dc78ece809898b9333e777ab5bf8b8
NataliaMigallon <natmigallon@gmail.com>
2021-04-09 14:11:50 +0200

    Add menu hambuger

---
+++ src/js/hambuger.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const menu = document.querySelector(".hamburger");
+
+// method
+function toggleMenu(event) {
+  this.classList.toggle("is-active");
+  document.querySelector(".header__nav").classList.toggle("is_active");
+  event.preventDefault();
+}
+
+// event
+menu.addEventListener("click", toggleMenu, false);
